3dc90d4abfee9fd2ba336fd3413731d62d364567
Emily Zhang <emilyzhang@netlify.com>
2021-11-15 09:02:06 -0800

    fix: builder handler types, thanks for pairing with me matt kane :)

--- src/function/handler.ts
+++ src/function/handler.ts
@@ -2,20 +2,14 @@ import type { Context } from './context'
 import type { Event } from './event'
 import type { Response, BuilderResponse } from './response'
 
-export interface HandlerCallback {
+export interface HandlerCallback<TResponse> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (error: any, response: Response): void
+  (error: any, response: TResponse): void
 }
 
-export interface Handler {
-  (event: Event, context: Context, callback: HandlerCallback): void | Response | Promise<Response>
+interface BaseHandler<TResponse> {
+  (event: Event, context: Context, callback: HandlerCallback<TResponse>): void | TResponse | Promise<TResponse>
 }
 
-export interface BuilderCallback {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (error: any, response: BuilderResponse): void
-}
-
-export interface BuilderHandler {
-  (event: Event, context: Context, callback: BuilderCallback): void | BuilderResponse | Promise<BuilderResponse>
-}
+export type Handler = BaseHandler<Response>
+export type BuilderHandler = BaseHandler<BuilderResponse>
