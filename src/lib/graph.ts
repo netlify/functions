d99e6d521ff59e8612d6a9d2da758af717b80260
Daniel Woelfel <dwwoelfel@gmail.com>
2022-08-12 11:20:14 -0700

    feat(graph): update graph helpers to work better with next.js

--- src/lib/graph.ts
+++ src/lib/graph.ts
@@ -5,8 +5,8 @@ import { Response } from '../function/response'
 
 import { getSecrets, NetlifySecrets } from './secrets_helper'
 // Fine-grained control during the preview, less necessary with a more proactive OneGraph solution
-export { getSecrets } from './secrets_helper'
-export { getNetlifyGraphToken, GraphTokenResponse, HasHeaders } from './graph_token'
+export { getSecrets, getSecretsForBuild } from './secrets_helper'
+export { getNetlifyGraphToken, getNetlifyGraphTokenForBuild, GraphTokenResponse, HasHeaders } from './graph_token'
 
 export interface ContextWithSecrets extends Context {
   secrets: NetlifySecrets
