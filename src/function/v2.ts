392f177d3602883aec2d34c1906dc6896a32abfb
Simon Knott <info@simonknott.de>
2023-09-22 08:51:17 +0200

    feat: publish `Config` type (#423)
    Adds the `Config` type for the v2 API.

--- src/function/v2.ts
+++ src/function/v2.ts
@@ -1,1 +1,13 @@
 export type { Context } from '@netlify/serverless-functions-api'
+
+type Path = `/${string}`
+
+type HTTPMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'OPTIONS'
+
+type CronSchedule = string
+
+export interface Config {
+  path?: Path | Path[]
+  method?: HTTPMethod | HTTPMethod[]
+  schedule?: CronSchedule
+}
