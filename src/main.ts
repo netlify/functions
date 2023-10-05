f424e8b843c79c4a96e6f8387942b5f88333e4d9
Lukas Holzer <lukas.holzer@netlify.com>
2023-10-05 17:10:47 +0200

    fix: fixes the ambient declarations on import

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
+import type { getNetlifyGlobal } from '@netlify/serverless-functions-api'
+
 export { builder } from './lib/builder.js'
 export { schedule } from './lib/schedule.js'
 export { stream } from './lib/stream.js'
 export * from './function/index.js'
+
+// Ambient type declarations
+declare global {
+  const Netlify: ReturnType<typeof getNetlifyGlobal>
+}
--- types/index.d.ts
+++
@@ -1,6 +0,0 @@
-import type { getNetlifyGlobal } from '@netlify/serverless-functions-api'
-
-// Ambient type declarations
-declare global {
-  const Netlify: ReturnType<typeof getNetlifyGlobal>
-}
