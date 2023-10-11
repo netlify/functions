656cfb5bba643836a804af0df579f8978d520b7a
Eduardo Bouças <mail@eduardoboucas.com>
2023-10-11 15:39:55 +0100

    feat: add `purgeCache` helper

---
+++ src/lib/purge_cache.ts
@@ -0,0 +1,46 @@
+import { env } from 'process'
+
+interface PurgeCacheOptions {
+  apiURL?: string
+  siteID?: string
+  tags: string[]
+  token?: string
+}
+
+export const purgeCache = ({ tags, ...overrides }: PurgeCacheOptions) => {
+  if (globalThis.fetch === undefined) {
+    throw new Error(
+      "`fetch` is not available. Please ensure you're using Node.js version 18.0.0 or above. Refer to https://ntl.fyi/functions-runtime for more information.",
+    )
+  }
+
+  const siteID = env.SITE_ID || overrides.siteID
+
+  if (!siteID) {
+    throw new Error(
+      'The Netlify site ID was not found in the execution environment. Please supply it manually using the `siteID` property.',
+    )
+  }
+
+  const token = env.NETLIFY_PURGE_TOKEN_1 || overrides.token
+
+  if (!siteID) {
+    throw new Error(
+      'The cache purge API token was not found in the execution environment. Please supply it manually using the `token` property.',
+    )
+  }
+
+  const apiURL = overrides.apiURL || 'https://api.netlify.com'
+
+  return fetch(`${apiURL}/api/v1/purge`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=utf8',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      site_id: siteID,
+      cache_tags: [tags],
+    }),
+  })
+}
