1f47afe1491b6ffa1c0537d875e2702bf111336b
Nathan Houle <nathan@nathanhoule.com>
2024-01-18 14:19:58 -0800

    fix: allow returning `Response` objects from function Handlers
    Per the [functions documentation][1], it seems like you should be able
    to return a standard `Response` object from function handlers. However,
    the existing types only allows a user to return a `HandlerResponse`
    object.
    
    This updates the Handler type to accept both types as a return value.
    
    (It looks like https://github.com/netlify/functions/pull/351 also
    addresses this issue, but that PR is a little stale at this point.)
    
    [1]: https://docs.netlify.com/functions/get-started/?fn-language=ts#synchronous-function

--- src/function/handler.ts
+++ src/function/handler.ts
@@ -2,13 +2,13 @@ import type { HandlerContext } from './handler_context.js'
 import type { HandlerEvent } from './handler_event.js'
 import type { HandlerResponse, BuilderResponse, StreamingResponse } from './handler_response.js'
 
-export interface HandlerCallback<ResponseType extends HandlerResponse = HandlerResponse> {
+export interface HandlerCallback<ResponseType extends HandlerResponse | Response = HandlerResponse> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (error: any, response: ResponseType): void
 }
 
 export interface BaseHandler<
-  ResponseType extends HandlerResponse = HandlerResponse,
+  ResponseType extends HandlerResponse | Response = HandlerResponse,
   C extends HandlerContext = HandlerContext,
 > {
   (event: HandlerEvent, context: C, callback?: HandlerCallback<ResponseType>): void | Promise<ResponseType>
@@ -18,7 +18,7 @@ export interface BackgroundHandler<C extends HandlerContext = HandlerContext> {
   (event: HandlerEvent, context: C): void | Promise<void>
 }
 
-export type Handler = BaseHandler<HandlerResponse, HandlerContext>
+export type Handler = BaseHandler<HandlerResponse | Response, HandlerContext>
 export type BuilderHandler = BaseHandler<BuilderResponse, HandlerContext>
 
 export interface StreamingHandler {
