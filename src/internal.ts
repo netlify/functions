c3f47247079e7de2f9f8303f154f4f914ec93589
Eduardo Bouças <mail@eduardoboucas.com>
2025-06-09 17:13:29 +0100

    chore: archive repo (#599)

--- src/internal.ts
+++
@@ -1,5 +0,0 @@
-// this file is exported as @netlify/functions/internal,
-// and only meant for consumption by Netlify Teams.
-// While we try to adhere to semver, this file is not considered part of the public API.
-
-export { systemLogger, LogLevel } from './lib/system_logger.js'
--- test/types/Netlify_Global.test-d.ts
+++
@@ -1,6 +0,0 @@
-import { expectAssignable } from 'tsd'
-
-// eslint-disable-next-line import/no-unassigned-import
-import '../../src/main.js'
-
-expectAssignable<{ env: unknown }>(Netlify)
